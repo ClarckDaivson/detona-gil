7e0449a1752048b4feac7bba5982c3c26cba338a
Daivson Natanaele <31487231+ClarckDaivson@users.noreply.github.com>
2024-10-04 13:47:52 -0300

    Atualização do audio
    Fiz atualização do audio

--- src/js/engine.js
+++ src/js/engine.js
@@ -29,7 +29,8 @@ function countDown() {
 }
 
 function playSound(audioName) {
-    let audio = new Audio(`./src/audios/${audioName}.m4a`);
+   // let audio = new Audio(`./src/audios/${audioName}.m4a`);
+    let audio = new Audio(`./src/audios/${audioName}.mp3`);
     audio.volume = 0.2;
     audio.play();
 }
